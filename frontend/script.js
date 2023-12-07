0fcb1bfe7026d02675f4859ad59984847f0d8b91
sanjaykumardev <duderaj1976@gmail.com>
2023-12-07 12:08:23 +0530

    project

---
+++ frontend/script.js
@@ -0,0 +1,67 @@
+function handleDetails() {
+  // Get form values
+  const employeenmae = document.getElementById("employeeName").value;
+  const dob = document.getElementById("dob").value;
+  const designation = document.getElementById("designation").value;
+  const salary = document.getElementById("salary").value;
+  const experience = document.getElementById("experience").value;
+  const employee_id = document.getElementById("employeeId").value;
+  const phone_number = document.getElementById("phoneNumber").value;
+  const address = document.getElementById("address").value;
+  const email = document.getElementById("email").value;
+
+  // // Check for empty required fields
+  if (!employeenmae || !dob || !designation || !employee_id || !phone_number || !email) {
+    alert("Please fill in all required fields:");
+    return; 
+}
+
+
+  // URL for the server to connect to the database:
+  const URL = "http://localhost:3000";
+
+  // Data to fetch in the backend:
+  const data = {
+      employeenmae,
+      dob,
+      designation,
+      salary,
+      experience,
+      employee_id,
+      phone_number,
+      address,
+      email
+  };
+
+  // Sending data to server
+  axios.post(URL + '/post', data)
+      .then(response => {
+          console.log('Data sent to the server:', response.data);
+          // Display the received data as a table row
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });
+
+  // Create a new table row
+  const table = document.getElementById("employeeTable");
+  const newRow = table.insertRow(table.length);
+
+  const cells = [employeenmae, dob, designation, salary, experience, employee_id, phone_number, address, email];
+  for (let i = 0; i < cells.length; i++) {
+      const cell = newRow.insertCell(i);
+      cell.innerHTML = cells[i];
+  }
+
+  // Reset input fields to empty strings
+  document.getElementById("employeeName").value = "";
+  document.getElementById("dob").value = "";
+  document.getElementById("designation").value = "";
+  document.getElementById("salary").value = "";
+  document.getElementById("experience").value = "";
+  document.getElementById("employeeId").value = "";
+  document.getElementById("phoneNumber").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("email").value = "";
+}
+
---
+++ server/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const port = 3000;
+
+// using the nodejs 
+app.use(express.json());
+app.use(cors());
+
+
+
+// MySQL Connection
+const connection = mysql.createConnection({
+  host:"127.0.0.1",
+  user:"root",
+  password:"sanjay007",
+  database:"employeeform",
+});
+
+
+// Endpoint to receive and store employee details
+    app.post('/post', (req, res) => {
+      const { employeenmae,dob,designation,salary,experience,employee_id,phone_number,address,email} = req.body;
+
+
+  connection.query('INSERT INTO form ( employeenmae, dob, designation, salary,experience,employee_id,phone_number,address, email) VALUES(?,?,?,?,?,?,?,?,?)', [ employeenmae, dob,designation, salary,experience, employee_id,phone_number,address,
+   email], (err, result) => {
+      if (err) {
+          console.error('Error inserting data into MySQL:', err);
+      }
+      console.log('Data inserted into MySQL successfully:', result);
+        res.json('successfully')
+    });
+  });
+
+// conntion to my sql
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+    return;
+  }
+  console.log('Connected to MySQL database!');
+});
+
+
+
+app.listen(port,()=>{
+  console.log(`server is running on ${port}`)
+});
+
+
+
